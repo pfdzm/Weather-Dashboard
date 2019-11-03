cd166e91660ff1785709200dec1bf9948699a219
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-11-03 23:53:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let app = document.querySelector("#app");
+
+function getWeather(city) {
+  let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9e32576aa8bc031eff72e8140283217f&units=metric`;
+  fetch(queryURL)
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      console.log(res);
+
+      let div = document.createElement("div");
+      div.classList.add("currentWeather");
+      div.innerHTML = ` <h4>${res.name}</h4>
+                        <p>Temperature: ${res.main.temp} C (${res.weather[0].description})</p>
+                        <p>Humidity: ${res.main.humidity}%</p>
+                        <p>Wind Speed: ${res.wind.speed} km/h</p>
+                        `;
+      app.append(div);
+    });
+}
+
+let form = document.querySelector("#search");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let city = document.querySelector("#city").value;
+  getWeather(city);
+});
