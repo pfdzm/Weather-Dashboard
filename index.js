ade4ea3e1d9a1c1a6de0b1f4a779a916a82870e4
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-11-07 10:33:16 +0100

    include bootstrap and some basic styling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let app = document.querySelector("#app");
+let target = document.querySelector("#target");
 
 function getWeather(city) {
   let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9e32576aa8bc031eff72e8140283217f&units=metric`;
@@ -8,21 +9,24 @@ function getWeather(city) {
     })
     .then(res => {
       console.log(res);
-
-      let div = document.createElement("div");
-      div.classList.add("currentWeather");
-      div.innerHTML = ` <h4>${res.name}</h4>
-                        <p>Temperature: ${res.main.temp} C (${res.weather[0].description})</p>
-                        <p>Humidity: ${res.main.humidity}%</p>
-                        <p>Wind Speed: ${res.wind.speed} km/h</p>
-                        `;
-      app.append(div);
+      target.innerHTML += `
+      <div class="col-sm-4">
+        <div class="card" >
+          <div class="card-body">
+            <h4 class="card-title">${res.name}</h4>
+            <p>Temperature: ${res.main.temp} C (${res.weather[0].description})</p>
+            <p>Humidity: ${res.main.humidity}%</p>
+            <p>Wind Speed: ${res.wind.speed} km/h</p>
+          </div>
+        </div>
+      </div>
+    `;
     });
 }
 
 let form = document.querySelector("#search");
 
-form.addEventListener("submit", e => {
+app.addEventListener("submit", e => {
   e.preventDefault();
   let city = document.querySelector("#city").value;
   getWeather(city);
